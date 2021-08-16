9d52f4307f741b2899aabc9c22c0522086f8a3db
DaveBosmans <dave.b11@hotmail.com>
2021-08-16 19:28:28 +0200

    interim commit
    interim commit

---
+++ DinosaurAPI/DinosaurAPI/BaseClasses/BaseUniqueID.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinosaurAPI.BaseClasses
+{
+
+    //Not used anywhere, but other classes will derrive from this class.
+    //TODO: How to make this class impossible to create objects.
+    public class BaseUniqueID
+    {
+        public string UniqueString { get; set; }
+    }
+}
---
+++ DinosaurAPI/DinosaurAPI/ImplementationClasses/GenerateStaminaLandDinov1.cs
@@ -0,0 +1,22 @@
+﻿using DinosaurAPI.HelperClasses;
+using DinosaurAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinosaurAPI.ImplementationClasses
+{
+    public class GenerateStaminaLandDinov1 : IStatsGenerator
+    {
+        //This class generates the stamina for a landdinosaur
+        public double GenerateStats()
+        {
+            GenerateRandomStatHelperLandDino generateStamina = new GenerateRandomStatHelperLandDino();
+
+            double responseHealth = generateStamina.GenerateRandomStaminaLandDino();
+
+            return responseHealth;
+        }
+    }
+}
